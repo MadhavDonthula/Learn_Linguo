a7842c499fd1c24a1f2c32030a21e4429046c3f2
MadhavDonthula <mdonthula98@gmail.com>
2024-08-21 21:24:22 -0700

    db fix2

--- voice_transcription/settings.py
+++ voice_transcription/settings.py
@@ -83,12 +83,11 @@ WSGI_APPLICATION = "voice_transcription.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'mydatabase.sqlite3',  # You can choose the file name here
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
