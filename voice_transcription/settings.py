d28e2e6fc0b42f435dca1d605d9e53e6aacf3fe6
MadhavDonthula <mdonthula98@gmail.com>
2024-08-21 21:30:35 -0700

    vercel database try2

--- voice_transcription/settings.py
+++ voice_transcription/settings.py
@@ -83,12 +83,20 @@ WSGI_APPLICATION = "voice_transcription.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+      'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'cCN9zP1fFbhB',
+        'HOST': 'ep-crimson-smoke-a41z43jy-pooler.us-east-1.aws.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+            "options": "endpoint=ep-crimson-smoke-a41z43jy"
     }
+      }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
