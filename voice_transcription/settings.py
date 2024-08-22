478cf0530b41cf2e7a47c0024446e778eb452c12
MadhavDonthula <mdonthula98@gmail.com>
2024-08-21 22:26:02 -0700

    fix static try2

--- voice_transcription/settings.py
+++ voice_transcription/settings.py
@@ -50,8 +50,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     #'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,7 +137,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Ensure that DEBUG is set to True in development:
 DEBUG = True
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
