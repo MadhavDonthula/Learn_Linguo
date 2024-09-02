873cb88a1731ee5cf1ad0fe35ee72af3d769d7a9
MadhavDonthula <mdonthula98@gmail.com>
2024-09-01 18:47:33 -0700

    part 2 of gptturbo3.5

--- voice_transcription/wsgi.py
+++ voice_transcription/wsgi.py
@@ -1,43 +1,43 @@
-"""
-WSGI config for voice_transcription project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voice_transcription.settings")
-
-application = get_wsgi_application()
-
-app = application
-
 # """
 # WSGI config for voice_transcription project.
+
 # It exposes the WSGI callable as a module-level variable named ``application``.
+
 # For more information on this file, see
 # https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 # """
 
 # import os
-# from whitenoise import WhiteNoise
+
 # from django.core.wsgi import get_wsgi_application
 
-# # Set the default Django settings module
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voice_transcription.settings")
 
-# # Initialize the WSGI application
 # application = get_wsgi_application()
 
-# # Wrap the application with WhiteNoise to serve static files
-# application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
-
-# # Add additional static files if needed
-
-# # Alias for the WSGI application
 # app = application
+
+"""
+WSGI config for voice_transcription project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+from whitenoise import WhiteNoise
+from django.core.wsgi import get_wsgi_application
+
+# Set the default Django settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voice_transcription.settings")
+
+# Initialize the WSGI application
+application = get_wsgi_application()
+
+# Wrap the application with WhiteNoise to serve static files
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
+
+# Add additional static files if needed
+
+# Alias for the WSGI application
+app = application
